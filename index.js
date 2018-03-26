00819340ba7c0a9f3fb22b768d04a8e80e3f5f41
Hannes Diercks <github@xiphe.net>
2018-03-26 22:24:31 +0200

    fix(main): fix export members

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+/* eslint-disable */
+
+'use strict';
+
 Object.defineProperty(exports, '__esModule', {
   value: true,
 });
 
-exports.default = require('./lib');
+var _lib = require('./lib');
+
+Object.defineProperty(exports, 'default', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_lib).default;
+  },
+});
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
